6aacd75023fa1c95b8f73b4efb048f21e2f3d0c2
DINESH NUVVULA <98139796+DineshNuvvula@users.noreply.github.com>
2023-02-11 23:48:19 +0530

    Update Tip_Calculator.py

--- Tip_Calculator.py
+++ Tip_Calculator.py
@@ -10,6 +10,6 @@ bill_with_tip = tip / 100 * bill + bill
 
 bill_per_person = bill_with_tip / people
 
-final_amt = round(bill_per_person, 2)
+final_amt = "{:.2f}".format(bill_per_person)
 
 print(f"Each person should pay: ${final_amt}") #f-string
